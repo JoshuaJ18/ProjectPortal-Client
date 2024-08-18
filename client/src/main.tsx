4ce07d2f5f1e1d8a30ae64bd8a8cf810ee25b907
Joshua Jones <joshuanjones18@gmail.com>
2024-08-17 17:25:13 -0700

    React Typescript + Vite + Bootstrap init

---
+++ client/src/main.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client'
+import Router from './Router'
+
+createRoot(document.getElementById('root')!).render(
+  <Router />
+);
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <p>TODO: Change this lol</p>
+    </div>  
+  );
+};
+
+export default HomePage;
